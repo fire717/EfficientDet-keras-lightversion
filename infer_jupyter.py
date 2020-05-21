8dda7776d558fd036f3b92f4a35cfeab97f9dcf8
fire717 <fire15@126.com>
2020-05-21 15:19:41 +0800

    add infer_jupyter

--- infer_jupyter.py
+++ infer_jupyter.py
@@ -5,11 +5,27 @@ import numpy as np # linear algebra
 import pandas as pd
 import glob
 #from utils import postprocess_boxes
-from model import efficientdet
-from utils.draw_boxes import draw_boxes
+from train import efficientdet
 from matplotlib import pyplot as plt
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+def draw_boxes(image, boxes, scores, labels, colors, classes):
+    for b, l, s in zip(boxes, labels, scores):
+        class_id = int(l)
+        class_name = classes[class_id]
+    
+        xmin, ymin, xmax, ymax = list(map(int, b))
+        score = '{:.4f}'.format(s)
+        color = colors[class_id]
+        label = '-'.join([class_name, score])
+    
+        ret, baseline = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+        cv2.rectangle(image, (xmin, ymax - ret[1] - baseline), (xmin + ret[0], ymax), color, -1)
+        cv2.putText(image, label, (xmin, ymax - baseline), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
 
 def py_nms(dets, scores, thresh, mode="Union"):
     """
