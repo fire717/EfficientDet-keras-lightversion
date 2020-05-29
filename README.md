afc602b7cb9ec8fc7ebfbfbba327e70d33b6c6fd
fire717 <fire15@126.com>
2020-05-29 14:51:52 +0800

    add wbf test result

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ This repo is a light version of the original one, remove some script.
 * add cocoapi eval
 
 ### Test
+```
 1. d2 bifpn=False pre_score_threshold=0.5 nms=0.5
+
 loss: 0.2411 - classification_loss: 0.1261 - regression_loss: 0.1151 - val_loss: 0.3281 - val_classification_loss: 0.1818 - val_regression_loss: 0.1463
 
 	localtrain: valmap9132   coco6810   
@@ -21,9 +23,42 @@ loss: 0.2411 - classification_loss: 0.1261 - regression_loss: 0.1151 - val_loss:
 	* pre_score_threshold=0.5 coco7430  board6298
 	* pre_score_threshold=0.4 coco7590  board6291
 
+	* py_nms(base): pre_score_threshold=0.5 nms=0.5 coco0.681
+    * Weighted-Boxes-Fusion nms: pre_score_threshold=0.5 nms=0.5 coco0.681
+	* Weighted-Boxes-Fusion wbf: pre_score_threshold=0.5 nms=0.5 coco0.681  board:0.6215
+							     pre_score_threshold=0.4 nms=0.5 coco0.693
+                                 pre_score_threshold=0.6 nms=0.5 coco0.664
+                                 pre_score_threshold=0.3 nms=0.5 coco0.699  board:0.5877
+                                 pre_score_threshold=0.2 nms=0.5 coco0.708
+                                 pre_score_threshold=0.1 nms=0.5 coco0.714  board:0.3883
+                                 pre_score_threshold=0.05 nms=0.5 coco0.714  
+                                 pre_score_threshold=0.0 nms=0.5 coco0.714
+                                 pre_score_threshold=0.1 nms=0.4 coco0.672
+                                 pre_score_threshold=0.1 nms=0.6 coco0.714
 
 
-2. 
+2. d5 512   
+	valmap0.85   pre_score_threshold=0.5 coco0.526
+				 pre_score_threshold=0.4 coco0.554
+                 pre_score_threshold=0.3 coco0.576
+	             pre_score_threshold=0.2 coco0.569
+
+3.d5 1280
+	valmap0.89   pre_score_threshold=0.5 coco0.154
+				 pre_score_threshold=0.7 coco0.135
+	             pre_score_threshold=0.2 coco0.013
+
+4.d4 1024 valmap0.9219
+	* score_threshold=0.5 nms coco0.693     wbf0.693
+    * score_threshold=0.4 nms coco0.703     wbf0.703
+	* score_threshold=0.3 nms coco0.684     wbf0.716
+
+
+5.d3 1024
+
+6.d2 1024
+
+```
 
 ### Run
 1. python setup_gen_overlap.py
@@ -36,4 +71,4 @@ loss: 0.2411 - classification_loss: 0.1261 - regression_loss: 0.1151 - val_loss:
 2. efficientd1 valmap0.9037 batchsize16
 3. efficientd2 valmap0.9148 batchsize16
 3. efficientd3 valmap0.9212 batchsize4
-4. efficientd4 valmap0.9XXX batchsize2
+4. efficientd4 valmap0.9219 batchsize2
--- computeScore.py
+++ computeScore.py
@@ -11,8 +11,9 @@ import json
 
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
+#from ensemble_boxes import *
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 
 def draw_boxes(image, boxes, scores, labels, colors, classes):
@@ -29,6 +30,15 @@ def draw_boxes(image, boxes, scores, labels, colors, classes):
         cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
         cv2.rectangle(image, (xmin, ymax - ret[1] - baseline), (xmin + ret[0], ymax), color, -1)
         cv2.putText(image, label, (xmin, ymax - baseline), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+
+
+
+
+
+
+
+
+
 
 
 def py_nms(dets, scores, thresh, mode="Union"):
@@ -133,6 +143,151 @@ def readData(data_path = "../data/wheat"):
     return data_dict
 
 
+def bb_intersection_over_union(A, B):
+    xA = max(A[0], B[0])
+    yA = max(A[1], B[1])
+    xB = min(A[2], B[2])
+    yB = min(A[3], B[3])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA) * max(0, yB - yA)
+
+    if interArea == 0:
+        return 0.0
+
+    # compute the area of both the prediction and ground-truth rectangles
+    boxAArea = (A[2] - A[0]) * (A[3] - A[1])
+    boxBArea = (B[2] - B[0]) * (B[3] - B[1])
+
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    return iou
+
+
+def prefilter_boxes(boxes, scores, labels, weights, thr):
+    # Create dict with boxes stored by its label
+    new_boxes = dict()
+    for t in range(len(boxes)):
+        for j in range(len(boxes[t])):
+            label = int(labels[t][j])
+            score = scores[t][j]
+            if score < thr:
+                break
+            box_part = boxes[t][j]
+            b = [int(label), float(score) * weights[t], float(box_part[0]), float(box_part[1]), float(box_part[2]), float(box_part[3])]
+            if label not in new_boxes:
+                new_boxes[label] = []
+            new_boxes[label].append(b)
+
+    # Sort each list in dict and transform it to numpy array
+    for k in new_boxes:
+        current_boxes = np.array(new_boxes[k])
+        new_boxes[k] = current_boxes[current_boxes[:, 1].argsort()[::-1]]
+
+    return new_boxes
+
+
+def get_weighted_box(boxes, conf_type='avg'):
+    """
+    Create weighted box for set of boxes
+    :param boxes: set of boxes to fuse 
+    :param conf_type: type of confidence one of 'avg' or 'max'
+    :return: weighted box
+    """
+
+    box = np.zeros(6, dtype=np.float32)
+    conf = 0
+    conf_list = []
+    for b in boxes:
+        box[2:] += (b[1] * b[2:])
+        conf += b[1]
+        conf_list.append(b[1])
+    box[0] = boxes[0][0]
+    if conf_type == 'avg':
+        box[1] = conf / len(boxes)
+    elif conf_type == 'max':
+        box[1] = np.array(conf_list).max()
+    box[2:] /= conf
+    return box
+
+
+def find_matching_box(boxes_list, new_box, match_iou):
+    best_iou = match_iou
+    best_index = -1
+    for i in range(len(boxes_list)):
+        box = boxes_list[i]
+        if box[0] != new_box[0]:
+            continue
+        iou = bb_intersection_over_union(box[2:], new_box[2:])
+        if iou > best_iou:
+            best_index = i
+            best_iou = iou
+
+    return best_index, best_iou
+
+
+def weighted_boxes_fusion(boxes_list, scores_list, labels_list, weights=None, iou_thr=0.55, skip_box_thr=0.0, conf_type='avg', allows_overflow=False):
+    '''
+    :param boxes_list: list of boxes predictions from each model, each box is 4 numbers. 
+    It has 3 dimensions (models_number, model_preds, 4)
+    Order of boxes: x1, y1, x2, y2. We expect float normalized coordinates [0; 1]
+    :param scores_list: list of scores for each model 
+    :param labels_list: list of labels for each model
+    :param weights: list of weights for each model. Default: None, which means weight == 1 for each model
+    :param intersection_thr: IoU value for boxes to be a match
+    :param skip_box_thr: exclude boxes with score lower than this variable  
+    :param conf_type: how to calculate confidence in weighted boxes. 'avg': average value, 'max': maximum value
+    :param allows_overflow: false if we want confidence score not exceed 1.0 
+    
+    :return: boxes: boxes coordinates (Order of boxes: x1, y1, x2, y2). 
+    :return: scores: confidence scores
+    :return: labels: boxes labels
+    '''
+
+    if weights is None:
+        weights = np.ones(len(boxes_list))
+    if len(weights) != len(boxes_list):
+        print('Warning: incorrect number of weights {}. Must be: {}. Set weights equal to 1.'.format(len(weights), len(boxes_list)))
+        weights = np.ones(len(boxes_list))
+    weights = np.array(weights)
+
+    if conf_type not in ['avg', 'max']:
+        print('Unknown conf_type: {}. Must be "avg" or "max"'.format(conf_type))
+        exit()
+
+    filtered_boxes = prefilter_boxes(boxes_list, scores_list, labels_list, weights, skip_box_thr)
+    if len(filtered_boxes) == 0:
+        return np.zeros((0, 4)), np.zeros((0,)), np.zeros((0,))
+
+    overall_boxes = []
+    for label in filtered_boxes:
+        boxes = filtered_boxes[label]
+        new_boxes = []
+        weighted_boxes = []
+
+        # Clusterize boxes
+        for j in range(0, len(boxes)):
+            index, best_iou = find_matching_box(weighted_boxes, boxes[j], iou_thr)
+            if index != -1:
+                new_boxes[index].append(boxes[j])
+                weighted_boxes[index] = get_weighted_box(new_boxes[index], conf_type)
+            else:
+                new_boxes.append([boxes[j].copy()])
+                weighted_boxes.append(boxes[j].copy())
+
+        # Rescale confidence based on number of models and boxes
+        for i in range(len(new_boxes)):
+            if not allows_overflow:
+                weighted_boxes[i][1] = weighted_boxes[i][1] * min(weights.sum(), len(new_boxes[i])) / weights.sum()
+            else:
+                weighted_boxes[i][1] = weighted_boxes[i][1] * len(new_boxes[i]) / weights.sum()
+        overall_boxes.append(np.array(weighted_boxes))
+
+    overall_boxes = np.concatenate(overall_boxes, axis=0)
+    overall_boxes = overall_boxes[overall_boxes[:, 1].argsort()[::-1]]
+    boxes = overall_boxes[:, 2:]
+    scores = overall_boxes[:, 1]
+    labels = overall_boxes[:, 0]
+    return boxes, scores, labels
 
 
 def IOU(Reframe,GTframe):
@@ -218,14 +373,14 @@ for data_map in raw_data:
 
 def evaluate(data_dict, data_path):
     print("start...")
-    phi = 2
+    phi = 4
     weighted_bifpn = False
     
     image_sizes = (512, 640, 768, 896, 1024, 1280, 1408)
     image_size = image_sizes[phi]
     classes = {0:"wheat"}
     num_classes = len(classes)
-    score_threshold = 0.6
+    score_threshold = 0.3
     colors = [np.random.randint(0, 256, 3).tolist() for _ in range(num_classes)]
     _, model = efficientdet(phi=phi,
                             num_classes=num_classes,
@@ -234,7 +389,7 @@ def evaluate(data_dict, data_path):
                            freeze_bn=False,
                            detect_quadrangle=False)
 
-    model_path = "models/d2_online.h5"
+    model_path = "models/d4_1024.h5"
     model.load_weights(model_path, by_name=True)
     print("finish load model")
     
@@ -246,27 +401,23 @@ def evaluate(data_dict, data_path):
         image = image[:, :, ::-1]
         src_image = image.copy()
         h, w = image.shape[:2]
-        #cv2.imwrite("pre_img1.jpg", image)
         image, scale = preprocess_image(image, image_size=image_size)
-        #cv2.imwrite("pre_img2.jpg", image)
-        # run network
-        #start = time.time()
         boxes, scores, labels = model.predict_on_batch([np.expand_dims(image, axis=0)])
-        # print("---raw ")
-        # print(boxes)
-        # print(scores)
-        # print(labels)
-
-        # b
         boxes_tmp, scores, labels = np.squeeze(boxes), np.squeeze(scores), np.squeeze(labels)
-        #print("cost time:",time.time() - start)
         boxes = boxes_tmp.copy()
         boxes= postprocess_boxes(boxes=boxes, scale=scale, height=h, width=w)
 
+
+        # nms
         boxes_keep_index = py_nms(boxes, scores, 0.5)
         boxes = boxes[boxes_keep_index]
         scores = scores[boxes_keep_index]
         labels = labels[boxes_keep_index]
+
+        # wbf
+        #boxes, scores, labels = nms([boxes], [scores], [labels], iou_thr=0.5)
+        #boxes, scores, labels = weighted_boxes_fusion([boxes], [scores], [labels], iou_thr=0.5)
+        #print(boxes.shape, scores.shape, labels.shape)
 
         indices = np.where(scores[:] > score_threshold)[0]
         # select those detections
